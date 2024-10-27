67bb2d18919ec297fb545794363b796acc341ff8
iiviie <divyanshverma158@gmail.com>
2024-10-27 14:49:50 +0530

    add views for authentication

---
+++ authentication/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from rest_framework_simplejwt.views import TokenRefreshView
+from .views import RegisterView, CustomTokenObtainPairView
+
+urlpatterns = [
+    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', CustomTokenObtainPairView.as_view(), name='login'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    # path('forgot-password/', ForgotPasswordView.as_view(), name='forgot_password'),
+]
--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework.permissions import AllowAny
+from .serializers import UserRegistrationSerializer, CustomTokenObtainPairSerializer
+
+class RegisterView(APIView):
+    permission_classes = (AllowAny,)
+
+    def post(self, request):
+        serializer = UserRegistrationSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({
+                "message": "User registered successfully",
+                "user": serializer.data
+            }, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class CustomTokenObtainPairView(TokenObtainPairView):
+    serializer_class = CustomTokenObtainPairSerializer
+
+# class ForgotPasswordView(APIView):
+#     permission_classes = (AllowAny,)
+
+#     def post(self, request):
+#         return Response({
+#             "message": "Password reset functionality will be implemented in future"
+#         }, status=status.HTTP_501_NOT_IMPLEMENTED)
