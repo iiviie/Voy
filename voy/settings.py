66b42f3ce0d60866350af07b6cc73cf08880ded0
iiviie <divyanshverma158@gmail.com>
2024-11-17 00:31:22 +0530

    update development

--- voy/settings.py
+++ voy/settings.py
@@ -98,25 +98,25 @@ WSGI_APPLICATION = 'voy.wsgi.application'
 #     }
 # }
 
-# db_config = dj_database_url.parse(config('DATABASE_URL'))
-# db_config['ENGINE'] = 'django.contrib.gis.db.backends.postgis'  # Force the engine to use PostGIS
-# DATABASES = {
-#     'default': db_config
-# }
+db_config = dj_database_url.parse(config('DATABASE_URL'))
+db_config['ENGINE'] = 'django.contrib.gis.db.backends.postgis'  # Force the engine to use PostGIS
+DATABASES = {
+    'default': db_config
+}
 
 
 
 #local postgresql database
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.contrib.gis.db.backends.postgis',
-        'NAME': 'voy_db',
-        'USER': 'voy_user',
-        'PASSWORD': 'voy_password',
-        'HOST': 'db',
-        'PORT': '5432',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.contrib.gis.db.backends.postgis',
+#         'NAME': 'voy_db',
+#         'USER': 'voy_user',
+#         'PASSWORD': 'voy_password',
+#         'HOST': 'db',
+#         'PORT': '5432',
+#     }
+# }
 GDAL_LIBRARY_PATH = os.getenv('GDAL_LIBRARY_PATH')
 GEOS_LIBRARY_PATH = os.getenv('GEOS_LIBRARY_PATH')
 
