6735df8a538f57bb21ef3804f4a1d677b8e0456b
tanvee30 <tanvee2331155@akgec.ac.in>
2024-11-22 17:08:18 +0530

    set urls

--- rides/urls.py
+++ rides/urls.py
@@ -63,18 +63,22 @@ urlpatterns = [
         CompletePaymentView.as_view(),
         name="complete-payment",
     ),
+    path(
+        'ride-history/',
+        RideHistoryView.as_view(),
+        name='ride-history'
+        ),
+    path(
+        'emissions-savings/<int:ride_id>/',
+        EmissionsSavingsView.as_view(),
+        name='emissions-savings'
+        ),
+    
+    
 
-    #driver patterns wil the these
-    path('driver/create/', CreateRideView.as_view(), name='driver-create-ride'),
-    path('driver/<int:ride_id>/status/', RideStatusView.as_view(), name='driver-update-ride-status'),
-    path('driver/requests/<int:ride_id>/', ListRideRequestsView.as_view(), name='driver-list-requests'),
-    path('driver/manage-request/<int:request_id>/', ManageRideRequestView.as_view(), name='driver-manage-request'),
-    #passenger patterns will be these
-    path('passenger/search/', FindRidesView.as_view(), name='passenger-search-rides'),
-    path('passenger/<int:ride_id>/request/', CreateRideRequestView.as_view(), name='passenger-request-ride'),
-    path('passenger/request/<int:request_id>/status/', PassengerStatusView.as_view(), name='passenger-update-status'),
-    path('ride-history/', RideHistoryView.as_view(), name='ride-history'),
-    path('emissions-savings/<int:ride_id>/', EmissionsSavingsView.as_view(), name='emissions-savings'),
+    
+    
+    
     
 ]
 
