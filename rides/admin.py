56aa3f9ef528a04848e3820c92d9323e1fe7476d
tanvee30 <tanvee2331155@akgec.ac.in>
2024-11-27 20:33:41 +0530

    update settings.py

--- rides/admin.py
+++ rides/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from rides.models import RideDetails,PassengerRideRequest,Rating, ChatMessage
+
 
 # Register your models here.
+
+
+
+admin.site.register(RideDetails)
+admin.site.register(PassengerRideRequest)
+admin.site.register(Rating)
+admin.site.register( ChatMessage)
